9dbc45c02995bcc87d2ccca09340377c59429050
Mohamed H. <mohamed7github@gmail.com>
2025-08-08 00:40:32 +0300

    feat: add HiveBoxProvider for managing Hive boxes

---
+++ lib/utils/helpers/hive_box_provider.dart
@@ -0,0 +1,11 @@
+import 'package:hive/hive.dart';
+
+class HiveBoxProvider {
+  static Future<Box<T>> getBox<T>(String boxName) async {
+    if(!Hive.isBoxOpen(boxName)) {
+      return await Hive.openBox<T>(boxName);
+    }
+
+    return Hive.box<T>(boxName);
+  }
+}
