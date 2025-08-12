ae47df13698e04a640c0e702a4f61d2b7c7a0ebf
Mohamed H. <mohamed7github@gmail.com>
2025-08-12 21:48:00 +0300

    refactor: update TBrandCard to use GestureDetector for navigation and improve code structure

--- lib/common/widgets/brands/brand_card.dart
+++ lib/common/widgets/brands/brand_card.dart
@@ -3,10 +3,12 @@ import 'package:t_store/common/widgets/custom_shapes/containers/rounded_containe
 import 'package:t_store/common/widgets/images/circular_image.dart';
 import 'package:t_store/common/widgets/texts/brand_title_with_verified_icon.dart';
 import 'package:t_store/features/shop/features/all_brands/domain/entities/brand_entity.dart';
+import 'package:t_store/features/shop/features/all_brands/presentation/pages/brand_products_page.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/enums.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
+import 'package:t_store/utils/helpers/navigation.dart';
 
 class TBrandCard extends StatelessWidget {
   const TBrandCard({super.key, this.showBorder = true, required this.brand});
@@ -16,39 +18,41 @@ class TBrandCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bool isDark = HelperFunctions.isDarkMode(context);
-    return TRoundedContainer(
-      padding: const EdgeInsets.all(TSizes.sm),
-      showBorder: showBorder,
-      backgroundColor: Colors.transparent,
-      child: Row(
-        children: [
-          Flexible(
-            child: TCircularImage(
-              image: brand.image,
-              backgroundColor: Colors.transparent,
-              imageColor: (isDark ? AppColors.light : AppColors.dark),
+    return GestureDetector(
+      onTap: () => context.pushPage(BrandProductsPage(brand: brand)),
+      child: TRoundedContainer(
+        padding: const EdgeInsets.all(TSizes.sm),
+        showBorder: showBorder,
+        backgroundColor: Colors.transparent,
+        child: Row(
+          children: [
+            Flexible(
+              child: TCircularImage(
+                image: brand.image,
+                backgroundColor: Colors.transparent,
+                imageColor: (isDark ? AppColors.light : AppColors.dark),
+              ),
             ),
-          ),
-          const SizedBox(width: TSizes.spaceBtwItems / 2),
-          
-          Expanded(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TBrandTitleWithVerifiedIcon(
-                  title: brand.name,
-                  brandTextSize: TextSizes.large,
-                ),
-                Text(
-                  '${brand.productCount} Products',
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.labelMedium,
-                ),
-              ],
-            ),
-          )
-        ],
+            const SizedBox(width: TSizes.spaceBtwItems / 2),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TBrandTitleWithVerifiedIcon(
+                    title: brand.name,
+                    brandTextSize: TextSizes.large,
+                  ),
+                  Text(
+                    '${brand.productCount} Products',
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
