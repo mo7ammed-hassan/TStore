3af33e7a131a87ff5226948cb816d6eb457b6562
Mohamed H. <mohamed7github@gmail.com>
2025-08-28 23:50:47 +0300

    feat(logic): create mapper to convert ProductCartItem to cart item

--- lib/features/shop/features/cart/data/mapper/product_cart_item_mapper.dart
+++ lib/features/shop/features/cart/data/mapper/product_cart_item_mapper.dart
@@ -1,5 +1,7 @@
 import 'package:t_store/features/shop/features/all_products/data/models/product_variation_model.dart';
+import 'package:t_store/features/shop/features/cart/data/models/cart_item_model.dart';
 import 'package:t_store/features/shop/features/cart/data/models/product_cart_item_model.dart';
+import 'package:t_store/features/shop/features/cart/domain/entities/cart_item_entity.dart';
 import 'package:t_store/features/shop/features/cart/domain/entities/product_cart_item_entity.dart';
 
 extension ProductCartItemMapper on ProductCartItemModel {
@@ -15,7 +17,7 @@ extension ProductCartItemMapper on ProductCartItemModel {
   }
 }
 
-extension EventModelMapper on ProductCartItemEntity {
+extension ProductCartItemModelMapper on ProductCartItemEntity {
   ProductCartItemModel toModel() {
     return ProductCartItemModel(
       id: id,
@@ -24,6 +26,26 @@ extension EventModelMapper on ProductCartItemEntity {
       price: price,
       variation: variation,
       brand: brand,
+    );
+  }
+}
+
+extension ProductCartItemToCartItemModelMapper on ProductCartItemModel {
+  CartItemModel toCartItemModel({int quantity = 1}) {
+    return CartItemModel(
+      id: '',
+      product: this,
+      quantity: quantity,
+    );
+  }
+}
+
+extension ProductCartItemToCartItemEntityMapper on ProductCartItemEntity {
+  CartItemEntity toCartItemModel({int quantity = 1}) {
+    return CartItemEntity(
+      id: '',
+      product: this,
+      quantity: quantity,
     );
   }
 }
